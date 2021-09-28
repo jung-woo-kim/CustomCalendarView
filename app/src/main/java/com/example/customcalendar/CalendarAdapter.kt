f06e3e854707c1ef125bd565c84eac73227f03b8
jungwookim <rlawjddn103@naver.com>
2021-09-28 18:56:14 +0900

    branch check

--- app/src/main/java/com/example/customcalendar/CalendarAdapter.kt
+++ app/src/main/java/com/example/customcalendar/CalendarAdapter.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import android.widget.Toast
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -26,7 +27,7 @@ class CalendarAdapter(context: Context, days: ArrayList<Date>, inputMonth: Int, 
     private var inputMonth = inputMonth - 1
     private val day1=days
     private val calender=calendar
-    @SuppressLint("UseCompatLoadingForDrawables")
+
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         day1.get(position).month
         var view = view
